7af45d081dc19130998635a994f4b9f766afee20
abhishekkumar-03 <abhishek_n_kumar@keybank.com>
2026-09-04 05:44:07 -0400

    Create validations.py

---
+++ validations.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from servicenow_client import get_validations
+
+def show_validations():
+  st.title("Validation Records")
+
+  df = get_validations()
+
+  status = st.selectbox(
+     "Filter by Status",
+     ["All","Failed","Open"]
+  )
+
+  if status != "All":
+    df = df[df["status"] == status]
+
+  st.dataframe(
+     df,
+     use_container_width=True
+  )
