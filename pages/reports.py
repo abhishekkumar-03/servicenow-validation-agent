39ca52e9443761c856979d26c84a33b143c69273
abhishekkumar-03 <abhishek_n_kumar@keybank.com>
2026-09-09 09:48:57 -0400

    Update reports.py

--- pages/reports.py
+++ pages/reports.py
@@ -1,4 +1,28 @@
 import streamlit as st
+import pandas as pd
+
+
 def show_reports(df):
-  st.title("📄 PDF Reports")
-  st.write("Generate and download reports")
+
+    st.title("📄 Reports")
+
+    st.subheader("Validation Report")
+
+    st.metric(
+        "Total Records",
+        len(df)
+    )
+
+    st.dataframe(
+        df,
+        use_container_width=True
+    )
+
+    csv = df.to_csv(index=False)
+
+    st.download_button(
+        label="⬇ Download CSV Report",
+        data=csv,
+        file_name="validation_report.csv",
+        mime="text/csv"
+    )
